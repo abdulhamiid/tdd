c6a05766eba0c31b16b14afc874f209f40182c24
Abdulhamid <adioabdulhamid@yahoo.com>
2022-09-08 19:36:03 +0100

    Create Test on Solver class methods

---
+++ spec/solver_spec.rb
@@ -0,0 +1,36 @@
+require '../solver'
+
+describe Solver do
+  before :each do
+    @solver = Solver.new
+  end
+
+  context 'when factorial method is called' do
+    it "returns the factorial of a given number" do
+      expect(@solver.factorial(0)).to eq(1)
+      expect(@solver.factorial(1)).to eq(1)
+      expect(@solver.factorial(4)).to eq(24)
+      expect(@solver.factorial(5)).to eq(120)
+    end
+  end
+
+  context 'when reverse method is called' do
+    it "returns the reverse value of a string" do
+      expect(@solver.reverse('hello')).to eq('olleh')
+      expect(@solver.reverse('abdulhamid')).to eq('dimahludba')
+      expect(@solver.reverse('felix')).to eq('xilef')
+      expect(@solver.reverse('microverse')).to eq('esrevorcim')
+    end
+  end
+
+  context 'when fizzbuzz method is called' do
+    it "returns \"fizz\", \"buzz\", or \"fizzbuzz\" when N is divisible by 3, 5 or 3 & 5 respectively else return N as a string" do
+      expect(@solver.fizzbuzz(3)).to eq('fizz')
+      expect(@solver.fizzbuzz(6)).to eq('fizz')
+      expect(@solver.fizzbuzz(5)).to eq('buzz')
+      expect(@solver.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(@solver.fizzbuzz(20)).to eq('buzz')
+      expect(@solver.fizzbuzz(22)).to eq('22')
+    end
+  end
+end
